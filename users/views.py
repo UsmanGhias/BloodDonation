f68505d5e1dd6584a83e2a4a63fa471ee41294de
Usman Ghias <usmanghias1@gmail.com>
2023-11-24 22:25:28 +0500

    Changes in views

--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
-
 @login_required
 def profile(request):
     try:
@@ -34,31 +33,3 @@ def profile(request):
         messages.error(request, f'Error: {e}')
         return render(request, 'users/profile.html')
 
-
-
-@login_required
-def profile(request):
-    try:
-        if request.method == 'POST':
-            u_form = UserUpdateForm(request.POST, instance=request.user)
-            p_form = ProfileUpdateForm(request.POST,
-                                       request.FILES,
-                                       instance=request.user.profile)
-            if u_form.is_valid() and p_form.is_valid():
-                u_form.save()
-                p_form.save()
-                messages.success(request, 'Your account has been updated, {}!'.format(request.user.username))
-                return redirect('profile')
-        else:
-            u_form = UserUpdateForm(instance=request.user)
-            p_form = ProfileUpdateForm(instance=request.user.profile)
-
-        context = {
-            'u_form': u_form,
-            'p_form': p_form
-        }
-
-        return render(request, 'users/profile.html', context)
-    except Exception as e:
-        messages.error(request, f'Error: {e}')
-        return render(request, 'users/profile.html')
